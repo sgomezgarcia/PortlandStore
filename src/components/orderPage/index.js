803b3789179ffdb030bafa1fc3f027e8aa130e1c
Sara <saragmzgrc@gmail.com>
2020-05-24 12:11:11 +0200

    static checkout screen

--- src/components/orderPage/index.js
+++ src/components/orderPage/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
-import { Icon, Button} from 'native-base';
+import { Icon, Button } from 'native-base';
 import { View, Text, Image } from 'react-native';
 
 import styles from './styles';
-import img1 from '../../assets/images/rastas.jpg';
 
 const OrderPage = ({ handleOrderScreen }) => (
   <View style={styles.menu}>
     <View style={styles.menuLine2}>
+      <Button style={styles.iconButton2} onPress={handleOrderScreen} title="hola">
+        <Icon
+          style={styles.edit}
+        >
+          <Text style={styles.editText}>edit</Text>
+        </Icon>
+      </Button>
       <Button style={styles.iconButton} onPress={handleOrderScreen} title="hola">
         <Icon
           ios="ios-close"
@@ -17,21 +23,33 @@ const OrderPage = ({ handleOrderScreen }) => (
       </Button>
     </View>
     <View>
-      <Text>shopping bag</Text>
-      <Text>product number</Text>
+      <Text style={styles.shoppingTitle}>shopping bag</Text>
+      <Text style={styles.productNumber}>product number</Text>
     </View>
-    <View>
-      <Image source={img1} />
-      <Text>skirt</Text>
-      <Text>price</Text>
+    <View style={styles.productContainer}>
+      <Image
+        source={{ uri: 'https://www.theblocklearning.com/wp-content/uploads/2018/09/512px-React-icon.svg.png' }}
+        style={styles.productImage}
+      />
+      <View style={styles.descriptionContainer}>
+        <View>
+          <Text style={styles.description}>mini blue skirt</Text>
+          <Text style={styles.size}>M</Text>
+        </View>
+        <Text style={styles.price}>39,95 EUR</Text>
+      </View>
     </View>
 
-    <View style={styles.menuLine}>
-      <Button style={styles.button} title="hola">
-        <Text style={styles.textNewIn}>new in</Text>
-      </Button>
+    <View style={styles.buyContainer}>
+      <View style={styles.menuLine}>
+        <Text style={styles.textTotal}>total 39,95 EUR</Text>
+        <Button style={styles.button} title="hola">
+          <Text style={styles.textAdd}>buy</Text>
+        </Button>
+      </View>
     </View>
   </View>
+
 );
 
 export default OrderPage;
--- src/components/orderPage/styles.js
+++ src/components/orderPage/styles.js
@@ -3,36 +3,139 @@ import { RFValue, RFPercentage } from 'react-native-responsive-fontsize';
 import { COLORS } from '../../utils/constants';
 
 const styles = StyleSheet.create({
+    button: {
+        backgroundColor: 'black',
+        margin: RFValue(20),
+        width: RFValue(180),
+        display: 'flex',
+        justifyContent: 'center',
+        elevation: 0,
+        height: RFValue(40)
+    },
+
+    textAdd: {
+        fontSize: RFValue(20),
+        textTransform: 'uppercase',
+        fontWeight: 'bold',
+        display: 'flex',
+        alignItems: 'center',
+        color: COLORS.white
+    },
+
     menu: {
         backgroundColor: COLORS.white,
         position: 'absolute',
         zIndex: 10000,
         height: RFPercentage(100),
-        width: RFPercentage(100),
-        paddingTop: RFValue(35),
-
+        paddingTop: RFValue(25),
     },
 
     closeIcon: {
         color: 'black',
         fontSize: RFValue(40),
         display: 'flex',
-        borderWidth: 0
+    },
 
+    edit: {
+        color: 'grey',
+        fontSize: RFValue(20),
+        textTransform: 'uppercase',
+    },
+
+    editText: {
+        fontWeight: 'bold'
+    },
+
+    description: {
+        textTransform: 'uppercase',
+        fontWeight: 'bold',
+        margin: RFValue(10),
+        fontSize: RFValue(15),
+        marginTop: RFValue(15),
+
+    },
+
+    textTotal: {
+        textTransform: 'uppercase',
+        fontWeight: 'bold'
+    },
+    price: {
+        textTransform: 'uppercase',
+        marginHorizontal: RFValue(10),
+        fontSize: RFValue(15),
+        marginTop: RFValue(15),
+
+    },
+
+    size: {
+        textTransform: 'uppercase',
+        margin: RFValue(10),
+        fontSize: RFValue(15),
+        fontWeight: 'bold'
+    },
+
+    productContainer: {
+        display: 'flex',
+        flexDirection: 'row',
+        marginTop: RFValue(30)
+    },
+
+    descriptionContainer: {
+        display: 'flex',
+        justifyContent: 'space-between'
+    },
+
+
+    productImage: {
+        width: 150,
+        height: 150,
+        marginLeft: RFValue(10),
+        marginTop: RFValue(20),
+        backgroundColor: 'black'
+     },
+
+    productNumber: {
+        textTransform: 'uppercase',
+        fontWeight: 'bold',
+        marginLeft: RFValue(10),
+        fontSize: RFValue(10),
+        color: 'grey'
+    },
+
+    shoppingTitle: {
+        textTransform: 'uppercase',
+        fontWeight: 'bold',
+        margin: RFValue(10),
+        fontSize: RFValue(20)
     },
 
     iconButton: {
         backgroundColor: 'white',
+        elevation: 0
+    },
+
+    iconButton2: {
+        backgroundColor: 'white',
+        elevation: 0
     },
 
     menuLine: {
-        backgroundColor: 'white',
+        alignItems: 'center',
+        display: 'flex',
+        justifyContent: 'center',
     },
+
     menuLine2: {
         backgroundColor: 'white',
         display: 'flex',
-        flexDirection: 'column',
-        marginLeft: RFValue(350)
+        flexDirection: 'row',
+        marginLeft: RFValue(300)
+    },
+
+    buyContainer: {
+        position: 'absolute',
+        bottom: 0,
+        alignSelf: 'center',
     },
 });
 
